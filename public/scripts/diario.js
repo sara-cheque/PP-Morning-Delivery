33a0a7f478978f83fa179908b0e3f36cf9cbe919
Sara Cheque <saracheque06@gmail.com>
2026-05-16 23:08:20 -0300

    inserindo relogio

--- public/scripts/diario.js
+++ public/scripts/diario.js
@@ -17,8 +17,6 @@ function novaTarefaFixa(){
     }
 
     inpNameToDo.value = ""
-
-    
 }
 
 function mostrarTarefaFixa(){
@@ -45,4 +43,23 @@ function excluirTarefaFixa(item){
 
 }
 
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time(){
+    let dateHoje = new Date();
+    let hr = dateHoje.getHours()
+    let min = dateHoje.getMinutes()
+    let sec = dateHoje.getSeconds()
+
+    if (hr < 10) {hr = '0' + hr}
+    if (min < 10) {min = '0' + min}
+    if (sec < 10) {sec = '0' + sec}
+
+    horas.innerHTML = hr
+    minutos.innerHTML = min
+    segundos.innerHTML = sec
+
+})
 
